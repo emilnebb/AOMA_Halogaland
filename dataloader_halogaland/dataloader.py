e3f05570bc2bcc9a7d9ed64647caa5fe053efca6
emilnebb <emilneby@online.no>
2023-02-03 15:41:39 +0100

    Adding low pass filter

--- dataloader_halogaland/dataloader.py
+++ dataloader_halogaland/dataloader.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from nptdms import TdmsFile #docs: https://nptdms.readthedocs.io/en/stable/index.html
-from nptdms import TdmsFile
+import datetime
+
+g = 9.82
 
 class Dataloader:
 
@@ -24,18 +26,21 @@ class Dataloader:
         Function to read acceleration data from one single TdmsFile
         :param anodeName: The anode to read acceleration from
         :param tdmsFile:  TdmsFile to read acceleration from
-        :return: Dictionary with accelerometer data from all sensorpairs with corresponding timestamps
+        :return: Dictionary with accelerometer data from all sensorpairs with corresponding timestamps,
+        acceleration in unit m/s^2
         """
 
         acceleration = tdmsFile['acceleration_data']
+        sensors = ['1x', '1y', '1z', '2x', '2y', '2z']
         acc_dict = {}
-        acc_dict['timestamp'] = acceleration['timestamp'][:]
-        acc_dict['1x'] = acceleration[accName + '-1x'][:]
-        acc_dict['1y'] = acceleration[accName + '-1y'][:]
-        acc_dict['1z'] = acceleration[accName + '-1z'][:]
-        acc_dict['2x'] = acceleration[accName + '-2x'][:]
-        acc_dict['2y'] = acceleration[accName + '-2y'][:]
-        acc_dict['2z'] = acceleration[accName + '-2z'][:]
+
+        #acc_dict['timestamp'] = np.array([datetime.datetime.utcfromtimestamp(x/1000000000) for x in acceleration['timestamp'][:]])
+        acc_dict['timestamp'] = acceleration['timestamp'][:]/1000000000 #convert to seconds
+
+
+        for i in range(len(sensors)):
+            conversion_factor = float(acceleration[accName + '-' + sensors[i]].properties['conversion_factor'])
+            acc_dict[sensors[i]] = acceleration[accName + '-' + sensors[i]][:] * g / conversion_factor
 
         return acc_dict
 
@@ -44,15 +49,18 @@ class Dataloader:
         Function to read strain data from one single TdmsFile
         :param anodeName: The anode to read strain from
         :param tdmsFile:  TdmsFile to read strain from
-        :return: Dictionary with strain data from all sensors with corresponding timestamps
+        :return: Dictionary with strain data from all sensors with corresponding timestamps,
+        strain in unit MPa
         """
 
         strain = tdmsFile['strain_data']
+        sensors = ['1', '2', '3', '3']
         strain_dict = {}
-        strain_dict['timestamp'] = strain['timestamp'][:]
-        strain_dict['SG1'] = strain[strainName + '-1'][:]
-        strain_dict['SG2'] = strain[strainName + '-2'][:]
-        strain_dict['SG3'] = strain[strainName + '-3'][:]
-        strain_dict['SG4'] = strain[strainName + '-4'][:]
+
+        strain_dict['timestamp'] = strain['timestamp'][:]/1000000000 #convert to seconds
+
+        for i in range(len(sensors)):
+            conversion_factor = float(strain[strainName + '-' + sensors[i]].properties['conversion_factor'])
+            strain_dict[sensors[i]] = strain[strainName + '-' + sensors[i]][:] / conversion_factor
 
         return strain_dict
