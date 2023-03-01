de38cda35fe1612d8baf7fd49a9b41743257a519
emilnebb <emilneby@online.no>
2023-03-01 16:03:05 +0100

    Add stabilization diagram plot function

--- dataloader_halogaland/processer.py
+++ dataloader_halogaland/processer.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
+import colordict
+color_values = list(colordict.ColorDict(norm=1).values())
 
 def low_pass(old_signal: np.ndarray, sampling_frequency, cutoff_frequency, filter_order) -> np.ndarray:
     """
@@ -55,3 +57,36 @@ def welch_plot(acc, sampling_frequency, Ndivisions):
     plt.grid()
     plt.legend()
     plt.show()
+
+def stabilization_diagram(acceleration, sampling_frequency, Ndivisions, frequencies, orders):
+
+    fig, ax =plt.subplots(figsize=(14, 6), dpi=300)
+
+    for cluster in range(frequencies.shape[0]):
+        ax.plot(frequencies[cluster], orders[cluster], marker='o', color=color_values[cluster +cluster*3])
+
+    ax.set_ylabel("Model order")
+    ax.set_xlabel('$f$ [Hz]')
+
+
+    Nwindow = np.ceil(len(acceleration) / Ndivisions)  # Length of window/segment
+
+    Nfft_pow2 = 2 ** (np.ceil(np.log2(Nwindow)))  # Next power of 2 for zero padding
+    dt = 1 / sampling_frequency  # Time step
+
+    # Call welch from scipy signal processing
+    f, Sx_welch = signal.welch(acceleration, fs=1 / dt, window='hann', nperseg=Nwindow, noverlap=None, nfft=Nfft_pow2,
+                               detrend='constant', return_onesided=True, scaling='density', axis=- 1, average='mean')
+
+    #Sx_est = np.abs((2.0 / len(acceleration)) * np.fft.rfft(acceleration))**2*1000
+    #f = np.fft.rfftfreq(acceleration.shape[0], dt)
+
+
+    ax2 = ax.twinx()
+    ax2.plot(f, Sx_welch, color='black', label='Welch spectrum of acceleration data', lw=0.5)
+    ax2.set_ylabel("PSD")
+
+    plt.grid()
+    plt.legend()
+
+    return fig
