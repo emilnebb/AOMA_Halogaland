546e42b241e59380b6791a32490eb49b1569b5d8
emilnebb <emilneby@online.no>
2023-03-02 17:26:07 +0100

    Improve stabilization diagram

--- dataloader_halogaland/processer.py
+++ dataloader_halogaland/processer.py
@@ -67,12 +67,12 @@ def stabilization_diagram(acceleration, sampling_frequency, Ndivisions, frequenc
         ax.scatter(all_freqs, all_orders, marker= 'o', color='grey', label='Discarded modes')
 
     for cluster in range(frequencies.shape[0]):
-        ax.plot(frequencies[cluster], orders[cluster], marker='o', color=color_values[cluster +cluster*3],
-                label=str(np.round(np.mean(frequencies[cluster]), decimals=3)) + " Hz")
+        ax.plot(frequencies[cluster], orders[cluster], marker='o', color=color_values[cluster +cluster*3]) #,
+                #label=str(np.round(np.mean(frequencies[cluster]), decimals=3)) + " Hz")
 
     ax.set_ylabel("Model order")
     ax.set_xlabel('$f$ [Hz]')
-    ax.legend(loc='lower center', bbox_to_anchor=(0.5, -5),)
+    #ax.legend()
 
     #Creating power spectral density in each direction of motion of the bridge
     Nwindow = np.ceil(len(acceleration) / Ndivisions)  # Length of window/segment
@@ -80,11 +80,15 @@ def stabilization_diagram(acceleration, sampling_frequency, Ndivisions, frequenc
     Nfft_pow2 = 2 ** (np.ceil(np.log2(Nwindow)))  # Next power of 2 for zero padding
     dt = 1 / sampling_frequency  # Time step
 
+    length = acceleration.shape[1]
+    length = int(length/3)
+    n_channels = int(length/2)
+
     B = 18.6
     # Transforming to find y, z, theta component for the sensor pair at the midspan
-    y = (acceleration[:, 20] + acceleration[:, 28]) / 2
-    z = (acceleration[:, 36] + acceleration[:, 44]) / 2
-    theta = (-acceleration[:, 36] + acceleration[:, 44]) / B
+    y = (acceleration[:, length + int(np.floor(length/4))] + acceleration[:, length + int(np.floor(length/4)) + n_channels]) / 2
+    z = (acceleration[:, length + int(np.floor(length/4)) + n_channels*2] + acceleration[:, length + int(np.floor(length/4)) + n_channels*3]) / 2
+    theta = (-acceleration[:, length + int(np.floor(length/4)) + n_channels*2] + acceleration[:, length + int(np.floor(length/4)) + n_channels*3]) / B
 
     # Call welch from scipy signal processing
     f, Sy_welch = signal.welch(y, fs=1 / dt, window='hann', nperseg=Nwindow, noverlap=None, nfft=Nfft_pow2,
@@ -101,10 +105,11 @@ def stabilization_diagram(acceleration, sampling_frequency, Ndivisions, frequenc
     ax2 = ax.twinx()
     ax2.plot(f, Sy_welch, color='black', label='$PSD\ y-direction$', lw=0.5)
     ax2.plot(f, Sz_welch, color='blue', label='$PSD\ z-direction$', lw=0.5)
-    ax2.plot(f, Stheta_welch*100, color='red', label=r'$PSD\ \theta-direction\ e2$', lw=0.5)
+    ax2.plot(f, Stheta_welch*10, color='red', label=r'$PSD\ \theta-direction\ e1$', lw=0.5)
     ax2.set_ylabel("PSD")
-    ax2.legend()
+    #ax2.legend()
 
+    plt.legend()
     plt.grid()
 
 
