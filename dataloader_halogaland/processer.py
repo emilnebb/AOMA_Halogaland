ffaf1a1cf6a6d9efeb0aeb41c1c1143de576252e
emilnebb <emilneby@online.no>
2023-03-02 16:36:42 +0100

    Improve stabilization diagram

--- dataloader_halogaland/processer.py
+++ dataloader_halogaland/processer.py
@@ -58,24 +58,40 @@ def welch_plot(acc, sampling_frequency, Ndivisions):
     plt.legend()
     plt.show()
 
-def stabilization_diagram(acceleration, sampling_frequency, Ndivisions, frequencies, orders):
+def stabilization_diagram(acceleration, sampling_frequency, Ndivisions, frequencies, orders,
+                          all_freqs = None, all_orders = None):
 
-    fig, ax =plt.subplots(figsize=(14, 6), dpi=300)
+    fig, ax =plt.subplots(figsize=(14, 5), dpi=300)
+
+    if len(all_freqs) > 0 and len(all_orders) > 0:
+        ax.scatter(all_freqs, all_orders, marker= 'o', color='grey', label='Discarded modes')
 
     for cluster in range(frequencies.shape[0]):
-        ax.plot(frequencies[cluster], orders[cluster], marker='o', color=color_values[cluster +cluster*3])
+        ax.plot(frequencies[cluster], orders[cluster], marker='o', color=color_values[cluster +cluster*3],
+                label=str(np.round(np.mean(frequencies[cluster]), decimals=3)) + " Hz")
 
     ax.set_ylabel("Model order")
     ax.set_xlabel('$f$ [Hz]')
+    ax.legend(loc='lower center', bbox_to_anchor=(0.5, -5),)
 
-
+    #Creating power spectral density in each direction of motion of the bridge
     Nwindow = np.ceil(len(acceleration) / Ndivisions)  # Length of window/segment
 
     Nfft_pow2 = 2 ** (np.ceil(np.log2(Nwindow)))  # Next power of 2 for zero padding
     dt = 1 / sampling_frequency  # Time step
 
+    B = 18.6
+    # Transforming to find y, z, theta component for the sensor pair at the midspan
+    y = (acceleration[:, 20] + acceleration[:, 28]) / 2
+    z = (acceleration[:, 36] + acceleration[:, 44]) / 2
+    theta = (-acceleration[:, 36] + acceleration[:, 44]) / B
+
     # Call welch from scipy signal processing
-    f, Sx_welch = signal.welch(acceleration, fs=1 / dt, window='hann', nperseg=Nwindow, noverlap=None, nfft=Nfft_pow2,
+    f, Sy_welch = signal.welch(y, fs=1 / dt, window='hann', nperseg=Nwindow, noverlap=None, nfft=Nfft_pow2,
+                               detrend='constant', return_onesided=True, scaling='density', axis=- 1, average='mean')
+    f, Sz_welch = signal.welch(z, fs=1 / dt, window='hann', nperseg=Nwindow, noverlap=None, nfft=Nfft_pow2,
+                               detrend='constant', return_onesided=True, scaling='density', axis=- 1, average='mean')
+    f, Stheta_welch = signal.welch(theta, fs=1 / dt, window='hann', nperseg=Nwindow, noverlap=None, nfft=Nfft_pow2,
                                detrend='constant', return_onesided=True, scaling='density', axis=- 1, average='mean')
 
     #Sx_est = np.abs((2.0 / len(acceleration)) * np.fft.rfft(acceleration))**2*1000
@@ -83,10 +99,13 @@ def stabilization_diagram(acceleration, sampling_frequency, Ndivisions, frequenc
 
 
     ax2 = ax.twinx()
-    ax2.plot(f, Sx_welch, color='black', label='Welch spectrum of acceleration data', lw=0.5)
+    ax2.plot(f, Sy_welch, color='black', label='$PSD\ y-direction$', lw=0.5)
+    ax2.plot(f, Sz_welch, color='blue', label='$PSD\ z-direction$', lw=0.5)
+    ax2.plot(f, Stheta_welch*100, color='red', label=r'$PSD\ \theta-direction\ e2$', lw=0.5)
     ax2.set_ylabel("PSD")
+    ax2.legend()
 
     plt.grid()
-    plt.legend()
+
 
     return fig
