aaccc565631b137be1f5bc9bc78f8a3e09e7702a
emilnebb <emilneby@online.no>
2023-05-15 18:12:25 +0200

    Add stadbiag from after Cov-SSI

--- dataloader_halogaland/plot.py
+++ dataloader_halogaland/plot.py
@@ -62,7 +62,7 @@ def stabilization_diagram(acceleration, sampling_frequency, Ndivisions, frequenc
 
     ax.set_ylabel("Model order")
     ax.set_xlabel('$f$ [Hz]')
-    #ax.legend()
+    ax.set_xticks(np.arange(0, 1, step=0.1))
 
     #Creating power spectral density in each direction of motion of the bridge
     Nwindow = np.ceil(len(acceleration) / Ndivisions)  # Length of window/segment
@@ -90,6 +90,71 @@ def stabilization_diagram(acceleration, sampling_frequency, Ndivisions, frequenc
 
     #Sx_est = np.abs((2.0 / len(acceleration)) * np.fft.rfft(acceleration))**2*1000
     #f = np.fft.rfftfreq(acceleration.shape[0], dt)
+
+
+    ax2 = ax.twinx()
+    ax2.plot(f, Sy_welch, color='black', label='$PSD\ y-direction$', lw=0.5)
+    ax2.plot(f, Sz_welch, color='blue', label='$PSD\ z-direction$', lw=0.5)
+    ax2.plot(f, Stheta_welch*10, color='red', label=r'$PSD\ \theta-direction\ e1$', lw=0.5)
+    ax2.set_ylabel("PSD")
+    #ax2.legend()
+
+    plt.legend()
+    plt.grid()
+
+    return fig
+
+def stabilization_diagram_cov_ssi(acceleration, sampling_frequency, Ndivisions,
+                          all_freqs, all_orders):
+    """
+    Creates a stabilization diagram with clustered poles labeled with the same color.
+    Optional takes the discarded poles and plots them as grey dots in the stabilization diagram.
+    Frequency content of the acceleration data is decomposed into horizontal, vertical and tortional
+    directions.
+    :param acceleration: 2d np.array of acceleration data
+    :param sampling_frequency: int
+    :param Ndivisions: int
+    :param frequencies: frequencies of the clustered poles, 2d np.array
+    :param orders: orders of the clustered poles, 2d np.array
+    :param all_freqs: frequecies of all poles created from cov-SSI, 1d np.array
+    :param all_orders: orders of all poles creaed from cov-SSI, 1d np.array
+    :return: figure including the stabilization diagram
+    """
+
+    fig, ax =plt.subplots(figsize=(14, 5), dpi=300)
+
+    if len(all_freqs) > 0 and len(all_orders) > 0:
+        ax.scatter(all_freqs, all_orders, marker= 'o', color='grey')
+
+    ax.set_ylabel("Model order")
+    ax.set_xlabel('$f$ [Hz]')
+    ax.set_xticks(np.arange(0, 1, step=0.1))
+    ax.set_xlim([0,1])
+
+    #Creating power spectral density in each direction of motion of the bridge
+    Nwindow = np.ceil(len(acceleration) / Ndivisions)  # Length of window/segment
+
+    Nfft_pow2 = 2 ** (np.ceil(np.log2(Nwindow)))  # Next power of 2 for zero padding
+    dt = 1 / sampling_frequency  # Time step
+
+    length = acceleration.shape[1]
+    length = int(length/3)
+    n_channels = int(length/2)
+
+    B = 18.6
+    # Transforming to find y, z, theta component for the sensor pair at the midspan
+    y = (acceleration[:, length + int(np.floor(length/4))] + acceleration[:, length + int(np.floor(length/4)) + n_channels]) / 2
+    z = (acceleration[:, length + int(np.floor(length/4)) + n_channels*2] + acceleration[:, length + int(np.floor(length/4)) + n_channels*3]) / 2
+    theta = (-acceleration[:, length + int(np.floor(length/4)) + n_channels*2] + acceleration[:, length + int(np.floor(length/4)) + n_channels*3]) / B
+
+    # Call welch from scipy signal processing
+    f, Sy_welch = signal.welch(y, fs=1 / dt, window='hann', nperseg=Nwindow, noverlap=None, nfft=Nfft_pow2,
+                               detrend='constant', return_onesided=True, scaling='density', axis=- 1, average='mean')
+    f, Sz_welch = signal.welch(z, fs=1 / dt, window='hann', nperseg=Nwindow, noverlap=None, nfft=Nfft_pow2,
+                               detrend='constant', return_onesided=True, scaling='density', axis=- 1, average='mean')
+    f, Stheta_welch = signal.welch(theta, fs=1 / dt, window='hann', nperseg=Nwindow, noverlap=None, nfft=Nfft_pow2,
+                               detrend='constant', return_onesided=True, scaling='density', axis=- 1, average='mean')
+
 
 
     ax2 = ax.twinx()
